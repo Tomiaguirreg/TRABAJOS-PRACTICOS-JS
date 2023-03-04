2c71152ec43cb2bf6abbea2292d911636ef63aaa
Your Name~ <you@example.com>
2023-03-04 14:32:42 -0300

    second commit

---
+++ tp1.js
@@ -0,0 +1,70 @@
+// EXERCISE 1
+
+// alert ("un mensaje");
+
+//EXERCISE 2
+
+// document.write ("hello world"); 
+
+//EXERCISE 3
+
+// document.write ("<br>",3+5);
+
+//EXERCISE 4
+
+// let userName = prompt("Escribe tu nombre");
+// document.write("Hola "+userName);
+
+// EXERCISE 5 
+
+// let unNumero = prompt("Escribe un numero");
+// let otroNumero = prompt ("Escribe otro numero");
+
+// document.write (parseInt(unNumero) + parseInt(otroNumero));
+
+//EXERCISE 6 
+
+// let unNumero = prompt("Escribe un numero");
+// let otroNumero = prompt ("Escribe otro numero");
+
+// if (unNumero > otroNumero) {
+//     document.write (unNumero)
+// } else {
+//     document.write (otroNumero)
+// };
+
+//EXERCISE 7
+
+// let n1 = prompt ("Escribe un numero");
+// let n2 = prompt ("Escribe un numero");
+// let n3 = prompt ("Escribe un numero");
+
+
+//  if (n1> n2 && n1 > n3) {
+
+//     document.write (n1);
+
+//  } else if (n2 > n3) {
+    
+//     document.write (n2);
+    
+//  } else {
+//     document.write (n3);
+//  };
+
+
+//EXERCISE 8
+
+// let numero = prompt ("Escriba un numero");
+
+// if (numero % 2 === 0) {
+//    document.write ("es divisible en 2");
+// } else {
+//     document.write ("no es divisible en 2");
+// };
+
+
+//EXERCISE 9 
+
+let frase = pormpt ("escribe una frase");
+
